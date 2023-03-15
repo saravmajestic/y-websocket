ad5e968c20abaeed60490571d369d0afea64844d
Saravanan <sarav.majestic@gmail.com>
2023-03-15 13:28:51 +0000

    fix: use separate port for websocket

--- bin/server.js
+++ bin/server.js
@@ -3,33 +3,33 @@
 /**
  * @type {any}
  */
-const WebSocket = require('ws')
-const http = require('http')
-const wss = new WebSocket.Server({ noServer: true })
-const setupWSConnection = require('./utils.js').setupWSConnection
+const WebSocket = require("ws");
+const http = require("http");
+const port = process.env.PORT || 1234;
+const wss = new WebSocket.Server({ port: 3001 });
+const setupWSConnection = require("./utils.js").setupWSConnection;
 
-const host = process.env.HOST || 'localhost'
-const port = process.env.PORT || 1234
+const host = process.env.HOST || "localhost";
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('okay')
-})
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  response.end("okay");
+});
 
-wss.on('connection', setupWSConnection)
+wss.on("connection", setupWSConnection);
 
-server.on('upgrade', (request, socket, head) => {
+server.on("upgrade", (request, socket, head) => {
   // You may check auth of request here..
   // See https://github.com/websockets/ws#client-authentication
   /**
    * @param {any} ws
    */
-  const handleAuth = ws => {
-    wss.emit('connection', ws, request)
-  }
-  wss.handleUpgrade(request, socket, head, handleAuth)
-})
+  const handleAuth = (ws) => {
+    wss.emit("connection", ws, request);
+  };
+  wss.handleUpgrade(request, socket, head, handleAuth);
+});
 
 server.listen(port, host, () => {
-  console.log(`running at '${host}' on port ${port}`)
-})
+  console.log(`running at '${host}' on port ${port}`);
+});
